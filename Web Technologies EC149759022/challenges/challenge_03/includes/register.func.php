3f47a9b8f2aaa6057e811badabc6eda5420c4ee9
criminus <neculaianisor@gmail.com>
2024-05-19 22:44:42 +0100

    Registration form completed

---
+++ Web Technologies EC149759022/challenges/challenge_03/includes/register.func.php
@@ -0,0 +1,62 @@
+<?php
+//Include config for database query
+require 'config.php';
+
+function registerUser(
+    $pdo, 
+    $first_name, 
+    $last_name, 
+    $password, 
+    $conf_password, 
+    $email
+)
+{
+    //Define empty errors array to hold any errors
+    $errors = [];
+
+    //Check if first name is empty
+    if (empty($first_name)) {
+        $errors[] = 'First Name field can\'t be empty!'; 
+    }
+
+    //Check if last name is empty
+    if (empty($last_name)) {
+        $errors[] = 'Last Name field can\'t be empty!'; 
+    }
+
+    //Check if passwords don't match
+    if ($password !== $conf_password) {
+        $errors[] = 'Password don\t match!';
+    }
+
+    //Validate email using FILTER_VALIDATE_EMAIL
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        $errors[] = 'The email address is not valid.';
+    }
+
+    //Check if this person has already an account with us:
+    $stmt = $pdo->prepare('SELECT first_name, last_name, email FROM users WHERE first_name = ? AND last_name = ? AND email = ?');
+    $stmt->execute([$first_name, $last_name, $email]);
+    $userCheck = $stmt->fetch(PDO::FETCH_ASSOC);
+
+    //Check here
+    if ($userCheck) {
+        $errors[] = 'There is already a customer with this details.';
+    }
+
+    //If there are no errors, we create the account with the given data:
+    if (empty($errors)) {
+        try {
+            //Hash password using password_hash instead of MD5
+            $hash_password = password_hash($password, PASSWORD_BCRYPT);
+            $stmt = $pdo->prepare("INSERT INTO users (first_name, last_name, pass, email, reg_date) VALUES (?, ?, ?, ?, NOW())");
+            $stmt->execute([$first_name, $last_name, $hash_password, $email]);
+        } 
+        catch (PDOException $e) {
+            $errors[] = 'Database error ' . $e->getMessage();
+        }
+    }
+
+    //Finally we return the errors
+    return $errors;
+}
--- Web Technologies EC149759022/challenges/challenge_03/signup.php
+++ Web Technologies EC149759022/challenges/challenge_03/signup.php
@@ -3,14 +3,38 @@
 require 'includes/config.php';
 //Load Twig
 require 'includes/autoload.php';
+//Load Register Function File
+require 'includes/register.func.php';
+
+//Start Session
+session_start();
+
+//Get errors generated while registration
+$errors = isset($_SESSION['errors']) ? $_SESSION['errors'] : [];
+unset($_SESSION['errors']);
 
 // Load Navigation
 $navigation = include 'navigation.php';
+
+//Handle form errors & submit
+$form_message = '';
+$form_message_type = '';
+
+if (isset($_GET['success']) && $_GET['success'] == 'true') {
+    $form_message = 'Your account has been created. You can now login!';
+    $form_message_type = 'success';
+} elseif (isset($_GET['error']) && $_GET['error'] == 'true') {
+    $form_message = 'Something went wrong. Please try again. If the problem persist, please contact the Administrator.';
+    $form_message_type = 'danger';
+}
 
 // Merge the navigation data with other data to pass to Twig
 $data = array_merge($navigation, [
     'sitename'              => 'MK Time',
     'pageTitle'             => 'Sign Up',
+    'errors'                => $errors,
+    'form_message'          => $form_message,
+    'form_message_type'     => $form_message_type,
 ]);
 
 // Render the template
