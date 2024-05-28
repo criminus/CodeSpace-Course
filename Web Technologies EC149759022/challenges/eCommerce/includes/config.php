2b09db034ae490b3e09cc1f14447af149fc1b51c
Criminus <neculaianisor@gmail.com>
2024-05-28 15:48:56 +0100

    updated config

--- Web Technologies EC149759022/challenges/eCommerce/includes/config.php
+++ Web Technologies EC149759022/challenges/eCommerce/includes/config.php
@@ -1,13 +1,13 @@
 <?php
 //Database connection using PDO
-$host = "localhost";
+$host = "127.0.0.1";
 $dbname = "codespace";
 $username = "root";
-$password = "";
+$password = "root";
 
 try {
     //Try the database connection
-    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
+    $pdo = new PDO("mysql:host=$host;port=8889;dbname=$dbname;charset=utf8", $username, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     die("Connection failed:" . $e->getMessage());
